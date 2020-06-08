3a57771e007672f109ee873793f10f1aa1d85fad
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-06-08 11:14:44 +0600

    list done

--- src/components/First.js
+++ src/components/First.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom'
 
 class First extends React.Component{
+state ={
+    lists:["a","b","c"]
+
+};
+listz=["a","b","c","d"]
+
+maps(){
+return <ul>{this.listz.map(list=><li>{list}</li>)}</ul>
+}
     render(){
         return <div>
+            {this.maps()}
             <h1></h1>
               <h1>Hi {this.props.name}</h1>
-    <p>Your Email: {this.props.email}</p>  
+                <p>Your Email: {this.props.email}</p>  
         </div>
     }
 }
